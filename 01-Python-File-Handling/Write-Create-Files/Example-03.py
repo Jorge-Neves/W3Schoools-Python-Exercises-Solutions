5fbe8b023036163258cdc8aec6312f1f812237ab
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-06-22 00:28:24 +0100

    Python Write/Create files Add Example 4, Update Example 3

--- 01-Python-File-Handling/Write-Create-Files/Example-03.py
+++ 01-Python-File-Handling/Write-Create-Files/Example-03.py
@@ -3,14 +3,15 @@ import os
 try:
     f = open("myExample0-3.txt", "x")
     f.close()
+    # "x" - will create a file, returns an error if the file exists.
 except:
     print("file myExample0-3.txt already exists in the current directory.")
     # We will now delete the file so that it will create the file every other time the program is ran.
     if os.path.exists("myExample0-3.txt"):
         os.remove("myExample0-3.txt")
-        print("myExample0-3.txt has been removed. Please rerun the the program to the file being created.")
+        print("myExample0-3.txt has been removed. Please rerun the program to see the file being created.")
     else:
-        print("An exception occured.")
+        print("An exception occurred.")
 
 
 if os.path.exists("myExample0-3.txt"):
--- 01-Python-File-Handling/Write-Create-Files/Example-04.py
+++ 01-Python-File-Handling/Write-Create-Files/Example-04.py
@@ -1,5 +1,15 @@
-f = open("demofile03.txt", "r")
+import os
 
-print(f.readline())
-
-# Will print the first line of the file
+if os.path.exists("myExample0-4.txt"):
+    print("file myExample0-3.txt already exists in the current directory.")
+    # We will now delete the file so that it will create the file every other time the program is ran.
+    os.remove("myExample0-4.txt")
+    if os.path.exists("myExample0-4.txt"):
+        print("An exception occurred")
+    else:
+        print("myExample0-4.txt has been removed. Please rerun the program to see the file being created.")
+else:
+    f = open("myExample0-4.txt", "w")
+    f.close()
+    # "x" - will create a file if the specified file does not exist.
+    print("if you check the current directory \n a text file with name myExample0-4 has been created.")
