357efac45d9fbf3c21531ffe624be16c17a413f9
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-06-22 07:35:35 +0100

    Rename Folder

---
+++ 00-Python-Tutorial/05-Python-Casting/Example-01.py
@@ -0,0 +1,11 @@
+# type casting is a way to specify a type on to a variable
+# 
+
+# os.system opens a new window
+
+f = open("example-01.txt")
+
+print(f.read())
+
+
+# .read() prints the content of the txt. file
