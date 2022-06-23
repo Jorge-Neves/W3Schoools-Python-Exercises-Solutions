41f7a0ba5ebbc7987515f3dba071227cc4dfb2fa
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-06-23 23:10:24 +0100

    Python Tutorial Arrays Example-02

---
+++ 00-Python-Tutorial/18-Python-Arrays/Example-02.py
@@ -0,0 +1,20 @@
+cars = ["Ford", "Volvo", "BMW"]
+
+print(cars)
+
+# This array holds three values: "Ford", "Volvo", and "BMW"
+
+x = cars[0]
+print(x)
+
+# x stores the value of the first array item
+
+y = cars[1]
+print(y)
+
+# y stores the value of the second array item
+
+z = cars[2]
+print(z)
+
+# z stores the value of the third array item
---
+++ 00-Python-Tutorial/18-Python-Arrays/Example-03.py
@@ -0,0 +1,9 @@
+# Python does not have built in support for Arrays
+# Python Lists can be used instead.
+# To work with arrays in Python one can import a library, like the NumPy Library
+
+cars = ["Ford", "Volvo", "BMW"]
+
+# The cars array holds 3 values at the same time
+
+print(cars)
