69ec9ba9cdce5566922b320878912f2a1302667c
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-06-24 00:13:34 +0100

    Python Tutorials Arrays Example-06

--- 00-Python-Tutorial/18-Python-Arrays/Example-06.py
+++ 00-Python-Tutorial/18-Python-Arrays/Example-06.py
@@ -1,9 +1,47 @@
-# Python does not have built in support for Arrays
-# Python Lists can be used instead.
-# To work with arrays in Python one can import a library, like the NumPy Library
-
 cars = ["Ford", "Volvo", "BMW"]
 
-# The cars array holds 3 values at the same time
+print(cars)
+
+cars.append("Honda")
 
 print(cars)
+# The append method append() adds an element at the end of a list
+
+cars.pop()
+
+List = ["Toyota", "Suzuki", "Fiat"]
+
+cars.append(List)
+
+print(cars)
+
+# Passing a list to the append() method will add the entire array as the last element
+# It is possible to add all elements of List to the cars array as separate elements with a loop
+
+cars.pop()
+
+print(cars)
+y = len(List)
+count = 0
+
+while count < y:
+    cars.append(List[count])
+    count += 1
+
+print(cars)
+
+# This while loop adds all elements from List individually to the end of the cars array
+
+for x in range(3):
+    cars.pop()
+
+print(cars)
+
+# This for loop returns the 3 items that the previous while loop appended
+
+for x in List:
+    cars.append(x)
+
+print(cars)
+
+# This for loop adds all elements from List individually to the end of the cars array
