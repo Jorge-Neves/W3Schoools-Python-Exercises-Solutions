83dd9618946b5d04fab1f01483dca7177d91314c
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-06-23 23:38:40 +0100

    Python Tutorial Arrays Example-05

--- 00-Python-Tutorial/18-Python-Arrays/Example-05.py
+++ 00-Python-Tutorial/18-Python-Arrays/Example-05.py
@@ -1,9 +1,29 @@
-# Python does not have built in support for Arrays
-# Python Lists can be used instead.
-# To work with arrays in Python one can import a library, like the NumPy Library
-
 cars = ["Ford", "Volvo", "BMW"]
 
-# The cars array holds 3 values at the same time
+for x in cars:
+    print(x)
 
-print(cars)
+# will print each item in the cars array
+
+print("-----")
+
+count = 0
+
+
+for y in cars:
+    if count % 2 == 0:
+        print(y)
+    count += 1
+
+count = 0
+
+# will print all items at even indexes in the cars array
+
+print("-----")
+
+for z in cars:
+    if count % 2 == 1:
+        print(z)
+    count += 1
+
+# will print all items at odd indexes in the cars array
