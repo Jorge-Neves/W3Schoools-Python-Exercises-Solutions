e230b59e9c34fcbe90d3504a6ff9e5b262d7ca68
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-07-19 23:34:25 +0100

    Python Math Example-01

---
+++ 00-Python-Tutorial/25-Python-Math/Example-01.py
@@ -0,0 +1,17 @@
+"""
+
+The min() and max() functions can be used to find the lowest and or the highest values in an iterable.
+
+print the minimum and max values of the following:
+
+x = [2, 3, 23, 54, 23]
+
+"""
+
+x = [2, 3, 23, 54, 23]
+
+y = min(x)
+z = max(x)
+
+print('minimum:', y)
+print('maximum:', z)
