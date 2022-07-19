4613fccd23ce28579533288fd13e8847b9164dfe
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-07-19 23:46:06 +0100

    Python Math Example-03

---
+++ 00-Python-Tutorial/25-Python-Math/Example-03.py
@@ -0,0 +1,21 @@
+"""
+
+print the following exponents:
+
+x = 7^3
+y = -2^5
+z = 87^2
+w = 23.4^7
+
+"""
+
+x = pow(7, 3)
+y = pow(-2, 5)
+z = pow(87, 2)
+w = pow(23.4, 7)
+
+print(x)
+print(y)
+print(z)
+print(w)
+
