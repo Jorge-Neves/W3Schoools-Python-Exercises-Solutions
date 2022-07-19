15bc8e767cdda744ba6318f6849e6d1047da3dff
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-07-19 23:42:00 +0100

    Python Math Example-02

---
+++ 00-Python-Tutorial/25-Python-Math/Example-02.py
@@ -0,0 +1,25 @@
+"""
+
+print the absolute values of the following:
+
+x = -7.25
+y = -4
+z = 87
+w = 23.4
+
+"""
+
+x = -7.25
+y = -4
+z = 87
+w = 23.4
+
+j = abs(x)
+k = abs(y)
+o = abs(z)
+m = abs(w)
+
+print(j)
+print(k)
+print(o)
+print(m)
