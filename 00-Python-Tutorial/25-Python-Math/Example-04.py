e3dcd817688f3ddc82ae69abe0d401a6d36c1381
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-07-25 23:51:53 +0100

    Add files via upload

---
+++ 00-Python-Tutorial/25-Python-Math/Example-04.py
@@ -0,0 +1,21 @@
+import math
+
+"""
+print the square roots of the following numbers:
+x = 7
+y = -2
+z = 87
+w = 23
+"""
+
+x = math.sqrt(7)
+y = math.sqrt(2)
+z = math.sqrt(87)
+w = math.sqrt(23)
+
+print(x)
+print(y)
+print(z)
+print(w)
+
+
