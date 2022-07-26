73ef38b1962a1295cc25c3b455b40fba86bb068e
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-07-26 23:55:25 +0100

    Add files via upload

---
+++ 00-Python-Tutorial/25-Python-Math/Example-05.py
@@ -0,0 +1,20 @@
+import math
+
+"""
+use the math ceiling and floor functions print the number upwards and downwards to its nearest integer for the following variables:
+x = 7.4
+y = -2.6
+
+"""
+
+x = math.ceil(7.4)
+y = math.floor(7.4)
+z = math.ceil(-2.6)
+w = math.floor(-2.6)
+
+print(x)
+print(y)
+print(z)
+print(w)
+
+
